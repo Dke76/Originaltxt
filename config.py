260c9cfc104a736edf9496954387d6295bd11948
Dke76 <156434263+Dke76@users.noreply.github.com>
2024-02-27 19:59:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ API_ID = API_ID = 20346550
 
 API_HASH = os.environ.get("API_HASH", "bc79c3bea7a626887bdc0871eecf0327")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6548002778:AAHjrz6DmmOqftU1cUEX3fikmnSWlojrBrE")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6469481034:AAGtJnTM81X6DAVPwvsnt_-rICBBirfaaz0")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
 OWNER = int(os.environ.get("OWNER", 5665231556))
 
-LOG = -1002128466713
+LOG = -1002003362897
 
 try:
     ADMINS=[]
